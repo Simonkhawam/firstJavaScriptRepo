193d487f55319ccc4c31b7627e6f8ea77fd59f9a
Simon Khawam <khawam.simon@me.com>
2023-10-31 23:22:52 +0200

    end of lesson 11 js

--- script.js
+++ script.js
@@ -12,35 +12,51 @@ const technologies = ["Javascript", "Python", "CSS", "HTML"];
 */
 
 const technologies = ["Javascript", "Python", "CSS", "HTML"];
-const containerEl = document.querySelector('#container');
-console.dir(containerEl);
-// const listEl = document.createElement('ul'); // <ul></ul>
-// console.dir(listEl);
-// containerEl.append(listEl);
+const containerEl = document.querySelector("#container");
+// console.dir(containerEl);
 
-// const firstElFromList = document.createElement('li');
-// firstElFromList.innerText = technologies[0];
-// listEl.append(firstElFromList);
+const listEl = document.createElement("ul"); //<ul></ul>
+containerEl.append(listEl);
 
-// for (let i = 1; i < technologies.length; i++) {
-//   const li = document.createElement('li'); // <li></li>
-//   li.innerText = technologies[i]; // <li>Python</li>
-//   listEl.append(li);
-// }
+const firstElFromList = document.createElement("li");
+firstElFromList.innerText = technologies[0];
+listEl.append(firstElFromList);
 
-// technologies.forEach( (technology) => {
-//   const li = document.createElement('li');
-//   li.innerText = technology;
-//   listEl.prepend(li);
-// })
+for (let i = 1; i < technologies.length; i++) {
+    const li = document.createElement("li"); // <li></li>
+    li.innerText = technologies[i];
+    listEl.append(li);
+}
 
-// technologies.forEach( (technology) => {
-//   const li = document.createElement('li');
-//   li.innerText = technology;
-//   listEl.append(li);
-// })
+// technologies.forEach((technology) => {
+//     const li = document.createElement("li");
+//     li.innerText = technology;
+//     listEl.append(li);
+// });
 
+// technologies.forEach((technology) => {
+//     const li = document.createElement("li");
+//     li.innerText = technology;
+//     // debugger;
+//     listEl.prepend(li);
+// });
 
+// //test example
+// const testQuerySelector = document.querySelector("#testId");
+// const testQuerySelectorAll = document.querySelectorAll("#testId");
+// const testElementById = document.getElementById("testId");
+// const testElementByClass = document.getElementsByClassName("testClass");
+// const testElementByTag = document.getElementsByTagName("div");
+
+// console.dir(testQuerySelector);
+
+// console.dir(testQuerySelectorAll);
+
+// console.dir(testElementById);
+
+// console.dir(testElementByClass[0]);
+
+// console.dir(testElementByTag);
 /*
 # Task 2
 
@@ -52,39 +68,37 @@ Starting from this colors array, create buttons dynamically;
 4. Add the button to the UI
 */
 
-// const colors = [
-//   {
-//     label: "Primary Button",
-//     color: "#ff0000",
-//     isPrimary: true
-//   },
-//   {
-//     label: "Click Me",
-//     color: "#00ff00",
-//     isPrimary: true
-//   },
-//   {
-//     label: "Yellow",
-//     color: "#fff00f",
-//     isPrimary: false
-//   },
-// ];
+const colors = [
+    {
+        label: "Primary Button",
+        color: "#ff0000",
+        isPrimary: true,
+    },
+    {
+        label: "Click Me",
+        color: "#0ff00",
+        isPrimary: true,
+    },
+    {
+        label: "yellow",
+        color: "#fff00f",
+        isPrimary: false,
+    },
+];
 
-// colors.forEach( color => {
-//   const buttonEl = document.createElement('button'); // <button></button>
-//   containerEl.append(buttonEl);
-//   buttonEl.textContent = color.label;
+colors.forEach((color) => {
+    const buttonEl = document.createElement("button"); // <button></button>
+    containerEl.append(buttonEl);
+    buttonEl.textContent = color.label;
 
-//   if (color.isPrimary) {
-//     buttonEl.innerHTML = `<span><a href='https://www.google.com/'>${color.label}</a></span>`;
-//     buttonEl.classList.add('primary-button');
-//   } else {
-//     buttonEl.style.backgroundColor = color.color;
-//     console.dir(buttonEl);
-//   }
-// })
-
-
+    if (color.isPrimary) {
+        buttonEl.innerHTML = `<span><a href='https://www.google.com/'>${color.label}</a></span>`;
+        buttonEl.classList.add("primary-button");
+    } else {
+        buttonEl.style.backgroundColor = color.color;
+        console.dir(buttonEl);
+    }
+});
 
 /**
  * Test Example
@@ -101,5 +115,3 @@ Starting from this colors array, create buttons dynamically;
 // console.dir(testElementById);
 // console.dir(testElementByClass[0]);
 // console.dir(testElementByTag);
-
-
